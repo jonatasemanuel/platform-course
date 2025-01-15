dd4a480e407d7cb98a96dfbd6110e7022cc855a6
joyboy- laptop <jonatasemanuel@gmail.com>
2025-01-14 22:24:29 -0300

    level transitions

--- main.lua
+++ main.lua
@@ -7,13 +7,16 @@ function love.load()
 
 	sprites = {}
 	sprites.playerSheet = love.graphics.newImage("sprites/playerSheet.png")
+	sprites.enemySheet = love.graphics.newImage("sprites/enemySheet.png")
 
 	local grid = anim8.newGrid(614, 564, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
+	local enemyGrid = anim8.newGrid(100, 79, sprites.enemySheet:getWidth(), sprites.enemySheet:getHeight())
 
 	animations = {}
 	animations.idle = anim8.newAnimation(grid("1-15", 1), 0.05)
 	animations.jump = anim8.newAnimation(grid("1-5", 2), 0.05)
 	animations.run = anim8.newAnimation(grid("1-15", 3), 0.05)
+	animations.enemy = anim8.newAnimation(enemyGrid("1-2", 1), 0.03)
 
 	wf = require("libraries/windfield")
 	world = wf.newWorld(0, 800, false)
@@ -24,13 +27,18 @@ function love.load()
 	world:addCollisionClass("Player")
 
 	require("player")
+	require("enemy")
 
 	-- danzerZone = world:newRectangleCollider(0, 550, 800, 50, { collision_class = "Danger" })
 	-- danzerZone:setType("static")
 
 	platforms = {}
 
-	loadMap()
+	currentLevel = "level-1"
+	flagX = 0
+	flagY = 0
+
+	loadMap(currentLevel)
 end
 
 function love.keypressed(key)
@@ -41,6 +49,10 @@ function love.keypressed(key)
 				player:applyLinearImpulse(0, -4000)
 			end
 		end
+	end
+
+	if key == "r" then
+		loadMap("level-1")
 	end
 end
 
@@ -65,24 +77,66 @@ function love.update(dt)
 	world:update(dt)
 	gameMap:update(dt)
 	playerUpdate(dt)
+	updateEnemies(dt)
 
 	local px, py = player:getPosition()
 	-- cam:lookAt(px, py)
 	cam:lookAt(px, love.graphics.getHeight() / 2)
+
+	local colliders = world:queryCircleArea(flagX, flagY, 10, { "Player" })
+	if #colliders > 0 then
+		if currentLevel == "leve-1" then
+			loadMap("level-2")
+		elseif currentLevel == "leve-2" then
+			loadMap("level-1")
+		end
+	end
 end
 
 function love.draw()
 	cam:attach()
 	gameMap:drawLayer(gameMap.layers["Tile Layer 1"])
-	-- world:draw()
+	world:draw()
 	playerDraw()
+	drawEnemies()
 	cam:detach()
 end
 
-function loadMap()
-	gameMap = sti("map/level-1.lua")
+function destroyAll()
+	local i = #platforms
+	while i > -1 do
+		if platforms[i] ~= nil then
+			platforms[i]:destroy()
+		end
+		table.remove(platforms, i)
+		i = i - 1
+	end
+	local i = #enemies
+	while i > -1 do
+		if enemies[i] ~= nil then
+			enemies[i]:destroy()
+		end
+		table.remove(enemies, i)
+		i = i - 1
+	end
+end
+
+function loadMap(mapName)
+	currentLevel = mapName
+	destroyAll()
+	player:setPosition(300, 100)
+	gameMap = sti("map/" .. mapName .. ".lua")
 
 	for i, obj in ipairs(gameMap.layers["Platform"].objects) do
 		spawnPlatform(obj.x, obj.y, obj.width, obj.height)
+	end
+
+	for i, obj in ipairs(gameMap.layers["Enemy"].objects) do
+		spawnEnemy(obj.x, obj.y)
+	end
+
+	for i, obj in ipairs(gameMap.layers["Flag"].objects) do
+		flagX = obj.x
+		flagY = obj.y
 	end
 end
